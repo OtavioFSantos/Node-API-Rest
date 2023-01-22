c7c133e5a7ca6db2e9f6676a1ccaa0b73c5dc3ef
OtavioFSantos <otaviofs01@gmail.com>
2023-01-21 21:22:32 -0300

    added more book routes

--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -3,6 +3,11 @@ import BookController from "../controllers/bookController.js";
 
 const router = express.Router();
 
-router.get("/books", BookController.listBooks);
+router
+  .get("/books", BookController.listBooks)
+  .get("/books/:id", BookController.listBookById)
+  .post("/books", BookController.addBook)
+  .put("/books/:id", BookController.updateBook)
+  .delete("books/:id", BookController.deleteBook);
 
 export default router;
