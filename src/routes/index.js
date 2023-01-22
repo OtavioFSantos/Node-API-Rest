2588e9313ffb70e0ab1c25560b3e05ec7c431bf6
OtavioFSantos <otaviofs01@gmail.com>
2023-01-21 21:23:12 -0300

    added authors

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import books from "./booksRoutes";
+import authors from "./authorsRoutes";
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
     res.status(200).send({ title: "Node" });
   });
 
-  app.use(express.json(), books);
+  app.use(express.json(), books, authors);
 };
 
 export default routes;
